6282e322d4c5cefa09c5cb9fdb263931477f45e5
yudairawan89 <yudairawan89@gmail.com>
2025-05-13 14:17:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -200,7 +200,7 @@ with realtime:
 with col_kanan:
     st.markdown("**IoT Smart Fire Prediction:**")
     from PIL import Image
-    image = Image.open("forestiot2.jpg")
+    image = Image.open("forestiot3.jpg")
     st.image(image.resize((430, 340)))
 
 
